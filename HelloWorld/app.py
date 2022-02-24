dde73e32ccf9aa81ab6c2f6f324259ccfdf93032
John Feng <atjohnfeng@gmail.com>
2022-02-23 17:39:43 -0800

    add additional practice

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -5,14 +5,37 @@
 # is_online = True
 # print(price + age)
 
-name = input('What is your name? ')
-print("Hello " + name)
+# name = input('What is your name? ')
+# print("Hello " + name)
 
-birth_year = input('Enter your birth year ')
-age = 2022 - int(birth_year)
-print(name + ' is ' + str(age) + " this year.")
+# birth_year = input('Enter your birth year ')
+# age = 2022 - int(birth_year)
+# print(name + ' is ' + str(age) + " this year.")
 
-float()
-bool()
-int()
-str()
+# float()
+# bool()
+# int()
+# str()
+
+#.lower() / .upper()
+# hello = 'Hello'
+# hello = hello.upper()
+# print(hello)
+
+#.find()
+# search_this = 'search this'
+# print(search_this.find('h'))
+
+#and or not
+# price = 15
+# print(price > 10 and price < 30)
+# print(price > 15 or price < 30)
+# print(not price > 15)
+
+# indentations matter! code block
+# temperature = 35
+# if temperature > 30:
+#     print("It's a hot day.")
+#     print("Drink plenty of water")
+# print("Yay!")
+
