001259bf73cc422316289277d30703c11b341436
John Feng <atjohnfeng@gmail.com>
2022-02-23 17:26:20 -0800

    add type conversion practice

---
+++ HelloWorld/app.py
@@ -0,0 +1,18 @@
+# age = 29
+# price = 19.95
+# first_name = 'John'
+# last_name = 'Smith'
+# is_online = True
+# print(price + age)
+
+name = input('What is your name? ')
+print("Hello " + name)
+
+birth_year = input('Enter your birth year ')
+age = 2022 - int(birth_year)
+print(name + ' is ' + str(age) + " this year.")
+
+float()
+bool()
+int()
+str()
