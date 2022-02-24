6a4cd82ef5bb1eeec5a433757efe7063b72eef3e
John Feng <atjohnfeng@gmail.com>
2022-02-23 20:26:33 -0800

    add additional notes

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -32,10 +32,43 @@
 # print(price > 15 or price < 30)
 # print(not price > 15)
 
-# indentations matter! code block
+# indentations matter! code block, if, elseif, else
 # temperature = 35
 # if temperature > 30:
 #     print("It's a hot day.")
 #     print("Drink plenty of water")
+# elif temperature > 30:
+#     print("It's a nice day today.")
+# elif temperature > 10:
+#     print("It's a bit cold")
+# else:
+#     print("It's cold")
 # print("Yay!")
 
+# has negative indexes like ruby
+# names = ["John", "Bob", "Mosh", "Apple"]
+# names[0] = "Jon"
+# print(names)
+# print(names[-1])
+# print(names[0:3])
+
+# numbers = [1, 2, 3, 4, 5]
+# print(10 in numbers) #returns false as 10 is not in numbers list
+# print(len(numbers))
+
+# for loops
+# for item in numbers:
+#     print(item)
+
+# i = 0
+# while i < len(numbers):
+#     print(numbers[i])
+#     i += 1
+
+# numbers = range(5, 10) # generates 5 to 9
+# for number in numbers:
+#     print(number)
+
+# numbers = range(5, 10, 2)  # generates 5 7 9
+# for number in numbers:
+#     print(number)
